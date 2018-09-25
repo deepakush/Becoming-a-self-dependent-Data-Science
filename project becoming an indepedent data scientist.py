25ce02ad543c2c3171d3b7aa18187e874e557d5e
Dip's <40426356+deepakush@users.noreply.github.com>
2018-09-25 15:19:17 +0530

    Update project becoming an indepedent data scientist.py

--- project becoming an indepedent data scientist.py
+++ project becoming an indepedent data scientist.py
@@ -1,39 +1,32 @@
 
-# coding: utf-8
 
-# # Assignment 4
+#Project
 # 
-# Before working on this assignment please read these instructions fully. In the submission area, you will notice that you can click the link to **Preview the Grading** for each step of the assignment. This is the criteria that will be used for peer grading. Please familiarize yourself with the criteria before beginning the assignment.
+# Before working on this assignment please read these instructions fully.Please familiarize your datasets with the criteria before beginning the assignment.
 # 
-# This assignment requires that you to find **at least** two datasets on the web which are related, and that you visualize these datasets to answer a question with the broad topic of **economic activity or measures** (see below) for the region of **Delhi, National Capital Territory of Delhi, India**, or **India** more broadly.
+# Q. This assignment requires that you to find **at least** two datasets on the web which are related, and that you visualize these datasets to answer a question with the broad topic of **measures** (see below) for the region of **Delhi, National Capital Territory of Delhi, India**, or **India** more broadly.
 # 
-# You can merge these datasets with data from different regions if you like! For instance, you might want to compare **Delhi, National Capital Territory of Delhi, India** to Ann Arbor, USA. In that case at least one source file must be about **Delhi, National Capital Territory of Delhi, India**.
-# 
-# You are welcome to choose datasets at your discretion, but keep in mind **they will be shared with your peers**, so choose appropriate datasets. Sensitive, confidential, illicit, and proprietary materials are not good choices for datasets for this assignment. You are welcome to upload datasets of your own as well, and link to them using a third party repository such as github, bitbucket, pastebin, etc. Please be aware of the Coursera terms of service with respect to intellectual property.
-# 
-# Also, you are welcome to preserve data in its original language, but for the purposes of grading you should provide english translations. You are welcome to provide multiple visuals in different languages if you would like!
-# 
+# Ans. I merged these datasets with data from different regions i Choose India and USA, two similar datasets! For instance, I want to compare ** India** to USA.
+
 # As this assignment is for the whole course, you must incorporate principles discussed in the first week, such as having as high data-ink ratio (Tufte) and aligning with Cairo’s principles of truth, beauty, function, and insight.
+
+# Here are the assignment steps:
 # 
-# Here are the assignment instructions:
+#  * State the region and the domain category that your data sets are about (e.g., **India** and USA, on **measures**).
+#  * State a question about the domain category and region that you identified as being interesting.
+#  * Must provide at least two links to available datasets. These could be links to files such as CSV or Excel files, or links to websites which might have data in tabular form, such as Wikipedia pages.
+#  * Must upload an image which addresses the research question you stated. In addition to addressing the question, this visual should follow Cairo's principles of truthfulness, functionality, beauty, and insightfulness.
+#  * Must contribute a short (1-2 paragraph) written justification of how your visualization addresses your stated research question.
 # 
-#  * State the region and the domain category that your data sets are about (e.g., **Delhi, National Capital Territory of Delhi, India** and **economic activity or measures**).
-#  * You must state a question about the domain category and region that you identified as being interesting.
-#  * You must provide at least two links to available datasets. These could be links to files such as CSV or Excel files, or links to websites which might have data in tabular form, such as Wikipedia pages.
-#  * You must upload an image which addresses the research question you stated. In addition to addressing the question, this visual should follow Cairo's principles of truthfulness, functionality, beauty, and insightfulness.
-#  * You must contribute a short (1-2 paragraph) written justification of how your visualization addresses your stated research question.
-# 
-# What do we mean by **economic activity or measures**?  For this category you might look at the inputs or outputs to the given economy, or major changes in the economy compared to other regions.
+# What do we mean by **measures**?  For this category you might look at the inputs or outputs to the given major changes in the measure compared to other regions.
 # 
 # ## Tips
 # * Wikipedia is an excellent source of data, and I strongly encourage you to explore it for new data sources.
 # * Many governments run open data initiatives at the city, region, and country levels, and these are wonderful resources for localized data sources.
 # * Several international agencies, such as the [United Nations](http://data.un.org/), the [World Bank](http://data.worldbank.org/), the [Global Open Data Index](http://index.okfn.org/place/) are other great places to look for data.
-# * This assignment requires you to convert and clean datafiles. Check out the discussion forums for tips on how to do this from various sources, and share your successes with your fellow students!
+# * This assignment requires you to convert and clean datafiles. 
 # 
-# ## Example
-# Looking for an example? Here's what our course assistant put together for the **Ann Arbor, MI, USA** area using **sports and athletics** as the topic. [Example Solution File](./readonly/Assignment4_example.pdf)
-
+# 
 # Link:
 # India https://data.worldbank.org/indicator/SP.DYN.AMRT.MA?contextual=default&end=2016&locations=IN&name_desc=false&start=1960&view=chart
 # 
